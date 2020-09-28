bab10f11b71888391fe0f5b784fb05f0ba67ede1
AaronFaltesek <aaron.faltesek@gmail.com>
2020-09-27 21:18:50 -0500

    AKF: conda environment defined. main file initialized. readme updated

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # photo_manager
 Python application to aid with managing by personal photos
+
+### Install conda env
+```conda env create -f environment.yaml```
---
+++ src/photo_manager_main.py
@@ -0,0 +1,14 @@
+
+
+
+if __name__ == "__main__":
+    """
+    System start function
+        
+    TODO: Pull settings
+    TODO: Setup logging
+    TODO: Setup CLI arg parse
+    """
+    print("------------ Main Function Start -------------------")
+
+    print("------------ Main Function End ---------------------")
